d98fda67c0538317bfd30d933ec1409c9fda247e
furkan-ylmz <140614294+furkan-ylmz@users.noreply.github.com>
2025-04-07 10:04:49 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,6 @@ class MyApp extends StatelessWidget {
         '/home': (context) => HomePage(),
         '/survey': (context) => SurveyPage(),
         '/statistics': (context) => StatisticsPage(),
-        //furkan
       },
     );
   }
